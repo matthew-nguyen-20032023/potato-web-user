5cad93a9bfe973c8eb19420a5f05adea3b12f3f7
Matthew <matthew.nguyen.20.03.2023@gmail.com>
2025-01-07 00:23:16 +0700

    fix: fix name and phone is optional

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -58,7 +58,6 @@ export function Register() {
             id="name"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-b-orange-300 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="name"
@@ -72,9 +71,8 @@ export function Register() {
             type="text"
             name="phone"
             id="phone"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-b-orange-300 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-xs text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-b-orange-300 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="phone"
