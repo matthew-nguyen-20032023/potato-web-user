641934f6d688b6f3cd2b1daf81aadf25de70b3f6
Matthew <matthew.nguyen.20.03.2023@gmail.com>
2025-02-11 22:23:41 +0700

    feat: footer and fixed header when scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,30 @@ import { RiShoppingBag3Line } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import UserMenu from "./UserMenu.tsx";
 import { TbEyeSpark } from "react-icons/tb";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="header bg-secondary-color pt-5">
+    <div
+      className={`header bg-secondary-color pt-5 ${
+        isScrolled ? "fixed z-10 top-0 w-full" : "relative"
+      }`}
+    >
       <div className="flex justify-center items-center">
         <button className="bg-secondary-color" type="button">
           <IoSearchOutline size={35} />
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -5,6 +5,7 @@ import Header from "../../components/Header.tsx";
 import Intro from "../../components/Intro.tsx";
 import Event from "../../components/Event.tsx";
 import { Cart } from "../cart/Cart.tsx";
+import Footer from "../../components/Footer.tsx";
 
 export function Main() {
   const location = useLocation();
@@ -20,7 +21,7 @@ export function Main() {
       <Cart />
       <ToastContainer />
       <Outlet />
-      {/*<Footer />*/}
+      <Footer />
     </div>
   );
 }
