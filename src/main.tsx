ee3b5649b217836e72c72e57582062324db9fbc7
Matthew <matthew.nguyen.20.03.2023@gmail.com>
2025-03-26 22:41:37 +0700

    feat: turn on strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "react-medium-image-zoom/dist/styles.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
-  <App />
-  // </StrictMode>,
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
