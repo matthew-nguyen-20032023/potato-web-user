b110a2fc4d672db8517fa3eb4328f4c952a5d440
Matthew <matthew.nguyen.20.03.2023@gmail.com>
2025-03-27 15:09:57 +0700

    fix: fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import jsxA11y from "eslint-plugin-jsx-a11y";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ["dist", "src/vite-env.d.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      parser: tseslint.parser,
-      parserOptions: { project: "./tsconfig.json", sourceType: "module" },
       globals: globals.browser,
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "jsx-a11y": jsxA11y,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -27,7 +23,6 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
-      "jsx-a11y/anchor-is-valid": "warn",
     },
   }
 );
