7f5d6f2f0643571f4176eeb89dc5eb1f5862cf03
Matthew <matthew.nguyen.20.03.2023@gmail.com>
2025-04-15 19:11:34 +0700

    feat: responsive for FAQ,contact and my order

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,13 +1,13 @@
 export default function Contact() {
   return (
     <div className="bg-list-product-color secondary-color p-12">
-      <h1 className="text-3xl font-semibold text-center">Contact</h1>
-      <p className="text-center mt-5">You can contact us at:</p>
-      <p className="text-center">Mew Mew Shop</p>
-      <p className="text-center">1234 Mew Mew Lane</p>
-      <p className="text-center">Mew York, NY 12345</p>
-      <p className="text-center">Phone: 123-456-7890</p>
-      <p className="text-center">
+      <h1 className="text-[3vh] font-semibold text-center">Contact</h1>
+      <p className="text-[1.5vh] text-center mt-5">You can contact us at:</p>
+      <p className="text-[1.5vh] text-center">Mew Mew Shop</p>
+      <p className="text-[1.5vh] text-center">1234 Mew Mew Lane</p>
+      <p className="text-[1.5vh] text-center">Mew York, NY 12345</p>
+      <p className="text-[1.5vh] text-center">Phone: 123-456-7890</p>
+      <p className="text-[1.5vh] text-center">
         Email:
         <a href="mailto:matthew.nguyen.20.03.2023@gmail.com"></a>
       </p>
